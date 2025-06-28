11e96e2ac12229573723a491da70a5730f316225
ayan <ayan787@gmail.com>
2025-06-28 22:15:55 +0530

    checking audio recieved or not

--- backend/endpoints/analyze.py
+++ backend/endpoints/analyze.py
@@ -1,6 +1,14 @@
-from fastapi import APIRouter
-from backend.routers.analyze_router import router
+from fastapi import UploadFile, APIRouter
+from backend.models.Speech_model import SpeechModel
+from backend.services.audio_conversion import audio_conversion
 
-@router.get(path="/analyze")
-async def analyze_voice():
-    return None
+router = APIRouter()
+
+@router.post(path="/",response_model=SpeechModel)
+async def analyze_voice(file: UploadFile):
+    audio_bytes = await file.read()
+    if audio_bytes:
+        return "audio received"
+    else:
+        return "Not received"
+    #converted_audio = audio_conversion(audio_bytes)
