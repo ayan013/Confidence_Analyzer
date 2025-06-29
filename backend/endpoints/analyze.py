c62a061e469d2d46a48670c3890b6c3075c39a03
ayan <ayan787@gmail.com>
2025-06-29 18:16:55 +0530

    Transcription done and tested

--- backend/endpoints/analyze.py
+++ backend/endpoints/analyze.py
@@ -4,6 +4,7 @@ from fastapi.responses import StreamingResponse
 from backend.models.Speech_model import SpeechModel
 from backend.services.audio_conversion import audio_conversion
 import io
+from backend.services.transcriber import transcription
 
 router = APIRouter()
 
@@ -11,4 +12,9 @@ router = APIRouter()
 async def analyze_voice(file: UploadFile = File(...)):
     audio_bytes = await file.read()
     converted_audio = audio_conversion(audio_bytes)
-
+    transcribed_audio = transcription(converted_audio)
+    if transcribed_audio:
+        print(transcribed_audio,flush=True)
+    else:
+        print("Failed",flush=True)
+    return None
