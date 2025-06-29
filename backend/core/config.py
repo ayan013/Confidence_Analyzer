8a92ff9dbec94d1382ee15d89a6b66e6aee96121
ayan <ayan787@gmail.com>
2025-06-29 18:11:43 +0530

    added proper pydantic settings and disabled dotenv

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,10 +1,10 @@
-from pydantic.env_settings import Basesettings
+from pydantic_settings import BaseSettings
 from typing import List
 from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
-class Settings(Basesettings):
+class Settings(BaseSettings):
     OPENAI_API_KEY: str
     CORS_ORIGINS: List[str] = ["*"]
 
