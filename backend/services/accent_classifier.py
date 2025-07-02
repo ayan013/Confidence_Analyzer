d1d8b09f1a994a96ba1cdf45c9c5735dad2c732a
ayan <ayan787@gmail.com>
2025-07-02 18:25:04 +0530

    removed test prints statements

--- backend/services/accent_classifier.py
+++ backend/services/accent_classifier.py
@@ -17,17 +17,13 @@ def detect_accent(audio_bytes: bytes):
             temp_path = temp.name
 
         audio_file = os.path.basename(temp_path)
-        #print(f"temp_path:- {temp_path}",flush=True)
         dest_dir = os.path.join(os.getcwd(), "audio")
-        #print(f"Dest: {dest_dir}",flush=True)
         dest_path=os.path.join(dest_dir,audio_file)
         shutil.move(temp_path,dest_path)
-        #print(f"✅ Moved to: {dest_path}", flush=True)
         relative_path = os.path.relpath(dest_path, os.getcwd())
-        #print(relative_path,flush=True)
         out_prob, score, idx, label = classifier.classify_file(relative_path)
 
         return label, score.item()
     except Exception as e:
-        print(f"❌ Accent detection failed: {e}")
+        print(f"Accent detection failed: {e}")
         return None
